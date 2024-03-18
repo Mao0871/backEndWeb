b4cc36313fc13e9eeaf41ce79ae9c28ff23af590
yingyi mao <yingyi_mao@cmu.ac.th>
2024-03-18 18:38:52 +0700

    v0.2.8FF

--- project/app.js
+++ project/app.js
@@ -2,41 +2,56 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mysql = require('mysql2');
-const _ = require('lodash');
-const path = require('path'); // 引入path模块
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-
-// 设置自定义视图目录
-app.set('views', path.join(__dirname));
-
-app.use(express.static("public"));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'd2002423m',
-  database: 'flywingsbackend'
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'd2002423m',
+    database: 'flywingsbackend'
 });
 
 connection.connect(err => {
-  if (err) {
-    console.error('Error connecting to MySQL: ' + err.stack);
-    return;
-  }
-  console.log('Connected to MySQL as id ' + connection.threadId);
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log(`Connected to MySQL as id ${connection.threadId}`);
 });
 
-app.get('/goods-management', (req, res) => {
-  connection.query('SELECT * FROM product', (err, results) => {
-    if (err) throw err;
-    res.render('goodsManagementMan', { products: results });
-  });
+// Root route - can redirect to a main page or render an index page
+app.get('/', (req, res) => {
+    res.redirect('/'); // Redirecting to the goods management page as an example
 });
+
+// Adjusted route to render EJS template for goods management
+app.get('/goods-management', (req, res) => {
+    connection.query('SELECT name, price, (`41` + `42` + `43` + `44` + `45` + `46`) AS inventory FROM product', (err, results) => {
+        if (err) {
+            console.error('Error fetching products: ' + err);
+            return res.status(500).send('Error fetching products');
+        }
+        res.render('backGoodsManaMan', { products: results });
+    });
+});
+
+app.get('/sell-history', (req, res) => res.render('BackOrderALL'));
+app.get('/best-seller', (req, res) => res.render('BackBestSeller'));
+app.get('/category-management', (req, res) => res.render('backCategoryMana'));
+app.get('/goods-woman', (req, res) => res.render('backGoodsManaWoman'));
+app.get('/goods-kids', (req, res) => res.render('backGoodsManakids'));
+app.get('/bill-summary', (req, res) => res.render('BackBillsummary'));
+app.get('/Detail', (req, res) => res.render('BackGoodsDetail'));
+app.get('/sub-category', (req, res) => res.render('BackSub_CategoryMana'));
+app.get('/', (req, res) => res.render('backendLogin.html'));
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
